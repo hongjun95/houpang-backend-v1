fe7c35981d7e4aae1d1d35a7e461e90e82f01c5d
zpskek <wjdghdwns0@gmail.com>
2021-09-01 14:35:21 +0900

    Setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import * as Joi from 'joi';
 
 @Module({
@@ -18,6 +20,28 @@ import * as Joi from 'joi';
     }),
     GraphQLModule.forRoot({
       playground: process.env.NODE_ENV !== 'production',
+      autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      ...(process.env.DATABASE_URL
+        ? { url: process.env.DATABASE_URL }
+        : {
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            // Postgresql은 localhost에 연결된 경우, password를 안 물어본다.
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+          }),
+
+      // TypeORM이 DB에 연결할 때 DB를 나의 모듈의 현재 상태로 migration한다는 뜻이다.
+      synchronize: process.env.NODE_ENV !== 'prod',
+
+      // DB에 무슨 일이 일어나는지 콘솔에 표시하는 거다.
+      logging:
+        process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
+      entities: [],
     }),
   ],
   controllers: [],
