61bb63c115a6481ee546b69cba214bf61b3a9236
zpskek <wjdghdwns0@gmail.com>
2021-09-03 13:44:44 +0900

    Create category entity

---
+++ src/categories/entities/category.entity.ts
@@ -0,0 +1,30 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsString, MinLength } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/common.entity';
+import { Product } from 'src/products/entities/product';
+import { Column, Entity, OneToMany } from 'typeorm';
+
+@InputType('CategoryInputType', { isAbstract: true })
+@ObjectType()
+@Entity()
+export class Category extends CoreEntity {
+  @Field((type) => String)
+  @Column({ unique: true })
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @Field((type) => String, { nullable: true })
+  @Column({ nullable: true })
+  @IsString()
+  coverImg: string;
+
+  @Field((type) => String)
+  @Column({ unique: true })
+  @IsString()
+  slug: string;
+
+  @Field((type) => [Product])
+  @OneToMany((type) => Product, (products) => products.category)
+  products: Product[];
+}
