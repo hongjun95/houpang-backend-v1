a96b170bd1dc4fffe6855fe73c1238175a9df5bd
zpskek <wjdghdwns0@gmail.com>
2021-09-04 16:27:21 +0900

    Create fav-list entity

---
+++ src/fav-lists/entities/favList.entity.ts
@@ -0,0 +1,18 @@
+import { InputType, ObjectType } from '@nestjs/graphql';
+import { CoreEntity } from 'src/common/entities/common.entity';
+import { Product } from 'src/products/entities/product';
+import { User } from 'src/users/entities/user.entity';
+import { Entity, JoinColumn, JoinTable, ManyToMany, OneToOne } from 'typeorm';
+
+@ObjectType('FavListInputType', { isAbstract: true })
+@InputType()
+@Entity()
+export class FavList extends CoreEntity {
+  @OneToOne(() => User)
+  @JoinColumn()
+  createdBy: User;
+
+  @ManyToMany(() => Product)
+  @JoinTable()
+  products: Product[];
+}
