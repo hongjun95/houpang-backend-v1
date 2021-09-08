6448a6719d7a83bf5116d2bf82b82cdfc1419e36
zpskek <wjdghdwns0@gmail.com>
2021-09-08 15:08:37 +0900

    Conver categories resolver to controller

---
+++ src/categories/categories.controller.ts
@@ -0,0 +1,76 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { Roles } from 'src/auth/roles.decorator';
+import { CategoriesService } from './categories.service';
+import {
+  CreateCategoryInput,
+  CreateCategoryOutput,
+} from './dtos/create-category.dto';
+import { DeleteCategoryOutput } from './dtos/delete-category.dto';
+import {
+  EditCategoryInput,
+  EditCategoryOutput,
+} from './dtos/edit-category.dto';
+import { GetAllCategoriesOutput } from './dtos/get-all-categories.dto';
+import {
+  GetProductsOnCategoryInput,
+  GetProductsOnCategoryOutput,
+} from './dtos/get-products-on-category.dto';
+
+@Controller('categories/')
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Get('')
+  @Roles(['Any'])
+  async getAllCategories(): Promise<GetAllCategoriesOutput> {
+    return this.categoriesService.getAllCategories();
+  }
+
+  @Get('products/:slug')
+  @Roles(['Any'])
+  async getProductsOnCategory(
+    @Param() getProductsOnCategoryInput: GetProductsOnCategoryInput,
+  ): Promise<GetProductsOnCategoryOutput> {
+    return this.categoriesService.getProductsOnCategory(
+      getProductsOnCategoryInput,
+    );
+  }
+
+  @Post('')
+  @Roles(['Admin'])
+  async createCategory(
+    @Body() createCategoryInput: CreateCategoryInput,
+  ): Promise<CreateCategoryOutput> {
+    return this.categoriesService.createCategory(createCategoryInput);
+  }
+
+  @Put(':categoryId/')
+  @Roles(['Admin'])
+  async editCategory(
+    @Body() body,
+    @Param('categoryId') categoryId,
+  ): Promise<EditCategoryOutput> {
+    const editCategoryInput: EditCategoryInput = {
+      ...body,
+      categoryId,
+    };
+    return this.categoriesService.editCategory(editCategoryInput);
+  }
+
+  @Delete(':categoryId/')
+  @Roles(['Admin'])
+  async deleteCategory(
+    @Param('categoryId') categoryId,
+  ): Promise<DeleteCategoryOutput> {
+    return this.categoriesService.deleteCategory({ categoryId });
+  }
+}
--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -4,9 +4,11 @@ import { Product } from 'src/products/entities/product';
 import { CategoriesResolver } from './categories.resolver';
 import { CategoriesService } from './categories.service';
 import { Category } from './entities/category.entity';
+import { CategoriesController } from './categories.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Category, Product])],
   providers: [CategoriesResolver, CategoriesService],
+  controllers: [CategoriesController],
 })
 export class CategoriesModule {}
