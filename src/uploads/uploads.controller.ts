2c0133bdb1076a4e967821a1a77fc40d7a0dd298
zpskek <wjdghdwns0@gmail.com>
2021-09-13 14:18:08 +0900

    Remove console.log on uploads controller

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -13,21 +13,15 @@ export class UploadsController {
   constructor(private readonly uploadService: UploadService) {}
 
   @Post('')
-  @UseInterceptors(FilesInterceptor('images', null, multerOptions))
-  uploadFile(@UploadedFiles() files: File[]) {
-    console.log('files');
-    console.log(files);
-
+  @UseInterceptors(FilesInterceptor('files', null, multerOptions))
+  uploadFile(@UploadedFiles() files) {
     const uploadedFiles: string[] = this.uploadService.uploadFiles(files);
-
-    console.log('uploadedFiles');
-    console.log(uploadedFiles);
 
     return {
       status: 200,
       message: '파일 업로드를 성공하였습니다.',
       data: {
-        files: uploadedFiles,
+        urls: uploadedFiles,
       },
     };
   }
