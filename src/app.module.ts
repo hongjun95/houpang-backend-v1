85a7a45fde7b6980b305392ef1b8b4eeae790d52
hongjun95 <hongjunjeong95@gmail.com>
2021-09-25 14:23:49 +0900

    Seperate graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
 
@@ -10,37 +9,18 @@ import { CategoriesModule } from './categories/categories.module';
 import { OrdersModule } from './orders/orders.module';
 import { ReviewsModule } from './reviews/reviews.module';
 import { LikesModule } from './likes/likes.module';
-import { TOKEN_KEY } from './common/common.constants';
 import { UploadsModule } from './uploads/uploads.module';
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { RefundsModule } from './refunds/refunds.module';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from './graphql/graphql.module';
 
 @Module({
   imports: [
     ConfigModule,
-    GraphQLModule.forRoot({
-      playground: process.env.NODE_ENV !== 'production',
-      autoSchemaFile: true,
-
-      context: ({ req }) => {
-        let token: string = '';
-        let authorization: string = '';
-        if (req.headers.hasOwnProperty(TOKEN_KEY)) {
-          authorization = req.headers[TOKEN_KEY];
-        }
-
-        if (authorization.includes('Bearer')) {
-          token = authorization.split(' ')[1];
-        }
-
-        return {
-          token,
-        };
-      },
-    }),
+    GraphQLModule,
     DatabaseModule,
     JwtModule.forRoot({
       privateKey: process.env.PRIVATE_KEY,
@@ -48,10 +28,11 @@ import { DatabaseModule } from './database/database.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-
     AuthModule,
-    UsersModule,
+
+    // apis
     CommonModule,
+    UsersModule,
     ProductsModule,
     CategoriesModule,
     OrdersModule,
@@ -59,7 +40,6 @@ import { DatabaseModule } from './database/database.module';
     ReviewsModule,
     UploadsModule,
     RefundsModule,
-    ConfigModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/graphql/graphql.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule as NestGraphQLModule } from '@nestjs/graphql';
+import { TOKEN_KEY } from 'src/common/common.constants';
+
+@Module({
+  imports: [
+    NestGraphQLModule.forRoot({
+      playground: process.env.NODE_ENV !== 'production',
+      autoSchemaFile: true,
+
+      context: ({ req }) => {
+        let token: string = '';
+        let authorization: string = '';
+        if (req.headers.hasOwnProperty(TOKEN_KEY)) {
+          authorization = req.headers[TOKEN_KEY];
+        }
+
+        if (authorization.includes('Bearer')) {
+          token = authorization.split(' ')[1];
+        }
+
+        return {
+          token,
+        };
+      },
+    }),
+  ],
+})
+export class GraphQLModule {}
