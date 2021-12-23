fdacfa5a5669d3e2f7930e967020555cbce3daf6
hongjunjeong95 <hongjunjeong95@gmail.com>
2021-12-23 10:17:45 +0900

    Add salt

--- src/apis/users/entities/user.entity.ts
+++ src/apis/users/entities/user.entity.ts
@@ -114,7 +114,9 @@ export class User extends CommonEntity {
   async hashPassword(): Promise<void> {
     if (this.password) {
       try {
-        this.password = await bcrypt.hash(this.password, 10);
+        const salt = await bcrypt.genSalt();
+
+        this.password = await bcrypt.hash(this.password, salt);
       } catch (e) {
         console.error(e);
         throw new InternalServerErrorException();
